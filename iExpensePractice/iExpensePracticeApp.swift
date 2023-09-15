b8b6933ccf76b0c370eadf5cf48a1bb0338f89de
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-15 01:11:51 -0400

    Initial Commit

---
+++ iExpensePractice/iExpensePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensePracticeApp.swift
+//  iExpensePractice
+//
+//  Created by Jordan Haynes on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpensePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
